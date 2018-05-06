04d4907a7e55faeb5a013156327a57c3f2d8fc5d
Leonardo Henrique <leohpr@gmail.com>
2018-05-06 17:47:00 -0300

    setting up routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('albums', 'Api\AlbumController')
+    ->only(['index', 'store', 'show', 'update', 'destroy']);
